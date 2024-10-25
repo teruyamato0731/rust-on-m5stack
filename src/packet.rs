51f1c6527bc24b4f449298f1a1dbe208b127fbfd
teruyamato0731 <teruyamato0731@gmail.com>
2024-10-25 12:57:54 +0000

    feat: Sensorのpacketを作成

--- src/packet.rs
+++ src/packet.rs
@@ -17,6 +17,13 @@ pub struct Control {
     pub u: i16,
 }
 
+#[derive(Debug, AsBytes, FromBytes, FromZeroes)]
+#[repr(C)]
+pub struct Sensor {
+    pub encoder: f32, // 駆動輪のオドメトリ
+    pub gyro: f32,    // ジャイロセンサの角速度
+}
+
 impl State {
     pub const SIZE: usize = std::mem::size_of::<Self>();
     pub const BUF_SIZE: usize = Self::SIZE + 2;
@@ -56,6 +63,22 @@ impl Control {
     pub fn current(&self) -> f32 {
         const K: f32 = Control::MAX as f32 / 20.0;
         self.u as f32 / K
+    }
+}
+
+impl Sensor {
+    pub const SIZE: usize = std::mem::size_of::<Self>();
+    pub const BUF_SIZE: usize = Self::SIZE + 2;
+
+    // cobsバイト列に変換
+    pub fn as_cobs(&self) -> [u8; Self::BUF_SIZE] {
+        let buf: [u8; Self::SIZE] = self.as_bytes().try_into().unwrap();
+        cobs_rs::stuff(buf, 0)
+    }
+    // cobsバイト列から復元
+    pub fn from_cobs(buf: &[u8; Self::BUF_SIZE]) -> Option<Self> {
+        let (cobs, _): ([u8; Self::SIZE], _) = cobs_rs::unstuff(*buf, 0);
+        Self::read_from(&cobs)
     }
 }
 
